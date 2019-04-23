c7486781d37b20c7a71cd3b4f2171abc58356072
Iwona Antonik <iwona.antonik@gmail.com>
2019-04-23 19:06:11 -0400

    Added client name to the output of upcoming sessions.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,12 +7,22 @@ module.exports = function (app) {
     app.get("/", function (req, res) {
         db.Session.findAll({
             limit: 5,
-            where: {
-                schedule_date: {
+            where:{
+                schedule_date:{
                     [Op.gt]: moment().format()
                 }
-            }
+            },
+            include: [
+                {
+                    model: db.Package, 
+                    include:[{
+                        model: db.Client
+                    }]
+                }
+               
+            ]
         }).then(function (response) {
+            // res.json(response)
             res.render("index", {schedule: response});
         });
     });
