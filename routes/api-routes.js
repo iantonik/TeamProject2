472a025c516326393fbd2099a378fbfb07eafec0
Iwona Antonik <iwona.antonik@gmail.com>
2019-04-23 15:44:32 -0400

    Upcoming Session: greater than today, limit 5 route. Updated index.handlebars to handle the results

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,10 +1,24 @@
 var db = require("../models");
+var moment = require('moment');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
-module.exports = function (app){
-    app.get("/", function(req, res){
-        db.Team.findAll({}).then(function(dbTeam){
-            //res.json(dbTeam)
-            res.render("index", {member: dbTeam})
-        })
-    })
+module.exports = function (app) {
+    app.get("/", function (req, res) {
+        db.Session.findAll({
+            limit: 5,
+            where: {
+                schedule_date: {
+                    [Op.gt]: moment().format()
+                }
+            }
+        }).then(function (response) {
+            res.render("index", {schedule: response});
+        });
+    });
+
+
 }
+
+
+
