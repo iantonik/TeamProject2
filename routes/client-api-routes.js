b1a0f60fbd47006f08c8524fe3d13d23de935921
drywitte <wittedanj@gmail.com>
2019-04-27 10:25:54 -0400

    able to view all clients and click to filter on single client

--- routes/client-api-routes.js
+++ routes/client-api-routes.js
@@ -5,15 +5,12 @@ const Op = Sequelize.Op;
 
 module.exports = function (app) {
 
-    app.get("/api/clients/:id", function(req, res) {
-        console.log("specific client");
-    });
 
     app.post("/api/clients/new", function(req, res) {
         console.log("heres a new client");
     });
 
-    app.put("/api/clients/edit", function(req, res) {
+    app.put("/api/clients/edit/:id", function(req, res) {
         console.log("editing a client");
     });
 
