44fb0464d41e78336a15d47d975d967d4181f937
Parisa Pahlevan <parisapahlevan@dyn-160-39-222-96.dyn.columbia.edu>
2019-04-27 11:11:41 -0400

    someMoreChanges

--- routes/client-api-routes.js
+++ routes/client-api-routes.js
@@ -4,24 +4,51 @@ const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
 module.exports = function (app) {
-
+    //  All clients witn the session belongs to them 
     app.get("/api/clients/all", function(req, res) {
         console.log("all clients");
+        db.Client.findAll({
+        where:{
+            id:{[Op.lt]: 10}}
+                // include: [db.Session]
+    })  .then(function(dbClient){
+            res.json(dbClient)
+        });
     });
 
     app.get("/api/clients/:id", function(req, res) {
         console.log("specific client");
-    });
+        // db.Client.findOne({
+        //     where: {
+        //       id: req.params.id
+        //     },
+        //     include: [db.session]
+        //   }).then(function(dbClient) {
+        //     res.json(dbClient);
+        //   });
+        });
+      
+ 
 
     app.post("/api/clients/new", function(req, res) {
         console.log("heres a new client");
+        // db.Client.create(req.body).then(function(dbClient) {
+        //     res.json(dbClient);
+        //   });
     });
 
     app.put("/api/clients/edit", function(req, res) {
         console.log("editing a client");
+        // db.Client.update(
+        //     req.body,
+        //     {
+        //       where: {
+        //         id: req.body.id
+        //       }
+        //     }).then(function(dbPost) {
+        //     res.json(dbClient);
+        //   });
+        // });
+
     });
-
-};
-
-
-
+}
