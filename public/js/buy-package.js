e749683f0d6c371e2c08500c23952828728d80f9
Iwona Antonik <iwona.antonik@gmail.com>
2019-04-30 11:53:24 -0400

    update client with new package id

--- public/js/buy-package.js
+++ public/js/buy-package.js
@@ -1,27 +1,45 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $(".buy-package").click(async () => {
+    $(".buy-package").click(async (e) => {
         var data = await getPackageList();
         var $dropdown = $('#selPkg');
-        $.each(data, function() {
-            if (!this.inactive)
-            {
+        $.each(data, function () {
+            if (!this.inactive) {
                 $dropdown.append($("<option />").val(this.id).text(`${this.workout_type}, Session count: ${this.session_count}, Price: $${this.price}`));
             }
         });
-        console.log(data);
+
+        var clientID = e.target.getAttribute('name');
+        $("#purchasing-clientID").attr("value", clientID);
     })
 
 
     var getPackageList = async () => {
         return new Promise((resolve, reject) => {
-            $.get('/api/package/all', function(data){
+            $.get('/api/package/all', function (data) {
                 resolve(data);
             }).fail((err) => {
                 reject(err);
             });
         });
 
+    }
+
+    $("#btn-purchase-package").click(function () {
+        var clientID =  $("#purchasing-clientID").val();
+        var pkgId = $('#selPkg').val();
+        console.log("client ID is", clientID);
+        console.log("pkg ID is", pkgId);
+        updateClient(clientID, pkgId);
+    });
+
+
+    var updateClient = function (id, pkgId) {
+        $.ajax({
+            method: "PUT",
+            url: `/api/clients/${id}`,
+            data: {packageId: pkgId}
+        }).then(location.reload());
     }
 
 });
--- routes/client-api-routes.js
+++ routes/client-api-routes.js
@@ -15,19 +15,33 @@ module.exports = function (app) {
           });
     });
 
-    app.put("/api/clients/edit/:id", function(req, res) {
-        console.log("editing a client");
-        db.Client.update(
-            req.body,
-            {
-                where: {
-                    id: req.params.id
-                }
-            }
-        ).then(function(dbClients) {
-            res.json(dbClients);
+    app.put("/api/clients/:id", function (req, res) {
+      console.log("updateClient function called... passthru to PUT");
+  
+      var id = req.params.id;
+      var clientObj = {};
+      if (req.body.first_name) clientObj.first_name = req.body.first_name;
+      if (req.body.last_name) clientObj.last_name = req.body.last_name;
+      if (req.body.email) clientObj.email = req.body.email;
+      if (req.body.address) clientObj.address = req.body.address;
+      if (req.body.phone_number) clientObj.phone_number = req.body.phone_number;
+      if (req.body.gender) clientObj.gender = req.body.gender;
+      if (req.body.age) clientObj.age = req.body.age;
+      if (req.body.weight) clientObj.weight = req.body.weight;
+      if (req.body.packageId) clientObj.PackageId = req.body.packageId;
+  
+      db.Client.update(clientObj, {
+          where: {
+            id: id
+          }
+        }).then(function (dbClient) {
+          res.json(dbClient);
         });
-    })
+    });
+
+
+
+
     app.delete("/api/clients/:id", function(req, res) {
         db.client.destroy({
           where: {
