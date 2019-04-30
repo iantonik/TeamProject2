ffd3b43e3e5815b7829cb3f8ef6cdac2a64f1853
drywitte <wittedanj@gmail.com>
2019-04-29 20:37:15 -0400

    basic cal working

--- public/js/all-sessions.js
+++ public/js/all-sessions.js
@@ -1,8 +1,19 @@
 $(function () {
-    $(".session").on("click", function() {
-        console.log("clicked");
-        var id = $(this).data("id");
-        console.log(id);
-        location.href = `/sessions/${id}`;
+    $(".edit-session").on("click", function() {
+        var session = $(this).data("id");
+        var calSelect = $(this).parent().append("<input class=\"cal-select\" type=>");
+        $(this).hide();
+        $(calSelect).flatpickr({
+            enableTime: true,
+            // inline: true,
+            onChange: function(dateStr) {
+                console.log(calSelect);
+                calSelect.val(dateStr)
+            }
+        });
     });
+
+    // $(".cal-select").on("click", function() {
+    //     console.log("cal clicked");
+    // });
 });
