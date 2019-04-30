3351a2a5a7ac28971834ab9ea1a0f1b2512548ec
drywitte <wittedanj@gmail.com>
2019-04-29 21:08:46 -0400

    sorta working

--- public/js/all-sessions.js
+++ public/js/all-sessions.js
@@ -1,19 +1,27 @@
 $(function () {
     $(".edit-session").on("click", function() {
-        var session = $(this).data("id");
-        var calSelect = $(this).parent().append("<input class=\"cal-select\" type=>");
+        var editButton = $(this);
+        var sessionID = $(this).data("id");
+        var inputID = sessionID + "-input";
+        var saveID = sessionID + "-save";
+        var calSelect = $(this).parent().append(`<input id=${inputID} class=\"cal-select\">`);
+        var saveButton = $(this).parent().append(`<button id=${saveID} class=\"cal-select\">Save</button>`);
         $(this).hide();
+
         $(calSelect).flatpickr({
             enableTime: true,
-            // inline: true,
             onChange: function(dateStr) {
                 console.log(calSelect);
-                calSelect.val(dateStr)
+                $(`#${inputID}`).val(dateStr),
+                $(`#${inputID}`).hide()
             }
         });
-    });
 
-    // $(".cal-select").on("click", function() {
-    //     console.log("cal clicked");
-    // });
+        $(saveButton).on("click", function() {
+            console.log(saveButton);
+            console.log(calSelect);
+            // $(editButton).show();
+            console.log("saved");
+        })
+    });
 });
